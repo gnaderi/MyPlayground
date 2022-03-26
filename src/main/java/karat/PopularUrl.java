37d6820a53cc1f1e4d735d5d635322aaeb7748ae
Ghodrat Naderi <naderi.ghodrat@gmail.com>
2022-03-26 20:55:03 +0000

    Another way of finding popular url in list of urls

--- src/main/java/karat/PopularUrl.java
+++ src/main/java/karat/PopularUrl.java
@@ -11,16 +11,28 @@ Let's say we wanted to extract the top N URLs instead of the single top URL. Can
 */
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.*;
 
 public class PopularUrl {
     public static void main(String[] argv) throws Exception {
 
         URL fileUrl = new URL("https://public.karat.io/content/q015/urls.txt");
+        findTopPopularUrls1(fileUrl,10);
+        System.out.println(" ===============================================================================================");
+        findTopPopularUrls2(fileUrl,10);
+
+    }
+
+    private static void findTopPopularUrls1(URL fileUrl, Integer topN) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(fileUrl.openStream()));
         Map<String, Integer> urlMap = new HashMap<>();
         String url;
@@ -28,9 +40,18 @@ public class PopularUrl {
             urlMap.merge(url, 1, Integer::sum);
 
         }
-        urlMap = urlMap.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+        urlMap = urlMap.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(topN).collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
 
- urlMap.keySet().stream().toList().subList(0,10).forEach(System.out::println);
+        urlMap.keySet().stream().toList().forEach(System.out::println);
+    }
 
+    private static void findTopPopularUrls2(URL fileUrl, Integer topN) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(fileUrl.openStream()));
+        List<String> urls = new ArrayList<>();
+        String url;
+        while ((url = br.readLine()) != null) urls.add(url);
+        urls.stream().collect(groupingBy(Function.identity(),Collectors.counting()))
+                .entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(topN).collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new))
+                .keySet().stream().toList().forEach(System.out::println);
     }
 }
